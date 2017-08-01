09ef0c290781f6989e45e9f644695a6685e3cf29
Stephen Ibanez <sibanez@g.hmc.edu>
2017-08-01 14:17:21 -0700

    Modified SUME simulation to include back pressure. Each output queue now drains at about 10 Gbps rather than 51 Gbps as it was before without back pressure.

--- lib/hw/std/cores/axis_sim_record_v1_0_0/hdl/axis_sim_record.v
+++ lib/hw/std/cores/axis_sim_record_v1_0_0/hdl/axis_sim_record.v
@@ -41,12 +41,16 @@
  *        axis_sim_record
  *
  *  Author:
- *        James Hongyi Zeng, David J. Miller, Georgina Kalogeridou
+ *        James Hongyi Zeng, David J. Miller, Georgina Kalogeridou,
+ *        modified by Stephen Ibanez
  *
  *  Description:
  *        Records traffic received from an AXI Stream master to an
  *        AXI grammar formatted text file.
  *
+ *        Added backpressure to limit the rate at which this module can
+ *        receive traffic. Count READY_COUNT cycles of receiving data
+ *        followed by NOT_READY_COUNT cycles of not receiving data. 
  */
 
 
@@ -57,7 +61,9 @@ module axis_sim_record
     // Master AXI Stream Data Width
     parameter C_S_AXIS_DATA_WIDTH = 256,
     parameter C_S_AXIS_TUSER_WIDTH = 128,
-    parameter OUTPUT_FILE = "../../stream_data_out.axi"
+    parameter OUTPUT_FILE = "../../stream_data_out.axi",
+    parameter READY_COUNT = 2,
+    parameter NOT_READY_COUNT = 8
 )
 (
     // Part 1: System side signals
@@ -69,7 +75,7 @@ module axis_sim_record
     input [((C_S_AXIS_DATA_WIDTH / 8)) - 1:0] s_axis_tkeep,
     input [C_S_AXIS_TUSER_WIDTH-1:0] s_axis_tuser,
     input s_axis_tvalid,
-    output s_axis_tready,
+    output reg s_axis_tready,
     input s_axis_tlast,
 
     output reg [10:0] counter,
@@ -80,36 +86,90 @@ module axis_sim_record
     integer bubble_count = 0;
     reg [8*2-1:0] terminal_flag;
     
-    assign s_axis_tready = 1;
-    
     initial begin
         f = $fopen(OUTPUT_FILE, "w");
         counter = 0;
+
+        state = READY;
+        ready_count = 0;
+        not_ready_count = 0;
+    end
+
+    reg [1:0] state;
+    reg [1:0] state_next;
+    // states
+    localparam READY = 0;
+    localparam NOT_READY = 1;
+
+    reg [31:0] ready_count;
+    reg [31:0] ready_count_next;
+    reg [31:0] not_ready_count;
+    reg [31:0] not_ready_count_next;
+     
+    always@(*) begin
+        state_next = state;
+        s_axis_tready = 0;
+        ready_count_next = ready_count;
+        not_ready_count_next = not_ready_count;
+
+        case(state)
+            READY: begin
+                s_axis_tready = 1;
+                if (ready_count == READY_COUNT-1) begin
+                    state_next = NOT_READY;
+                    ready_count_next = 0;
+                end
+                else if (s_axis_tvalid) begin
+                    ready_count_next = ready_count + 1;
+                end
+            end
+
+            NOT_READY: begin
+                s_axis_tready = 0;
+                if (not_ready_count == NOT_READY_COUNT-1) begin
+                    state_next = READY;
+                    not_ready_count_next = 0;
+                end
+                else begin
+                    not_ready_count_next = not_ready_count + 1;
+                end
+            end
+        endcase
+
     end
 
     always @(posedge axi_aclk) begin
+        state <= state_next;
+        ready_count <= ready_count_next;
+        not_ready_count <= not_ready_count_next;
+    end
+
+
+    always @(posedge axi_aclk) begin
         if (s_axis_tvalid == 1'b1) begin
-            if (bubble_count != 0) begin
-                $fwrite(f, "*%0d\n", bubble_count);
-                bubble_count <= 0;
+            if (s_axis_tready == 1'b1) begin
+                if (bubble_count != 0) begin
+                    $fwrite(f, "*%0d\n", bubble_count);
+                    bubble_count <= 0;
+                end
+                if (s_axis_tlast == 1'b1) begin
+                    terminal_flag = ".";
+	            counter <= counter + 1;
+	            activity_rec <= 1;
+                end
+                else begin
+                    terminal_flag = ",";
+	            activity_rec <= 1;
+                end
+                
+                $fwrite(f, "%x, %x, %x%0s # %0d ns\n",
+                                  s_axis_tdata,
+                                  s_axis_tkeep,
+                                  s_axis_tuser,
+                                  terminal_flag,
+                                  $time
+                                  ); 
             end
-            if (s_axis_tlast == 1'b1) begin
-                terminal_flag = ".";
-		counter <= counter + 1;
-		activity_rec <= 1;
-            end
-            else begin
-                terminal_flag = ",";
-		activity_rec <= 1;
-            end
-            
-            $fwrite(f, "%x, %x, %x%0s # %0d ns\n",
-                              s_axis_tdata,
-                              s_axis_tkeep,
-                              s_axis_tuser,
-                              terminal_flag,
-                              $time
-                              ); 
         end
         else begin
             bubble_count <= bubble_count + 1;
