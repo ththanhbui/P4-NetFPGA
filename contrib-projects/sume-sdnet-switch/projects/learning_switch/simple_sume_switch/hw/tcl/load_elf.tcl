ae0c63773d47e8c3b3549712e1e77bb83b444256
Stephen Ibanez <sibanez@g.hmc.edu>
2019-02-13 01:52:33 -0800

    Updated the learning_switch project to build SDK project and load ELF files

--- contrib-projects/sume-sdnet-switch/projects/learning_switch/simple_sume_switch/hw/tcl/load_elf.tcl
+++ contrib-projects/sume-sdnet-switch/projects/learning_switch/simple_sume_switch/hw/tcl/load_elf.tcl
@@ -37,18 +37,22 @@
 
 
 set design [lindex $argv 0] 
+set ws "SDK_Workspace"
 
 # open project
-open_project project/${design}.xpr
+puts "\nOpening $design XPR project\n"
+open_project project/$design.xpr
 
 set bd_file [get_files -regexp -nocase {.*sub*.bd}]
-set elf_file ../sw/embedded/project/SDK_Workspace/${design}/Debug/${design}.elf
+set elf_file ../sw/embedded/$ws/$design/app/Debug/app.elf
 
+puts "\nOpening $design BD project\n"
 open_bd_design $bd_file
 
-# insert acceptance_test elf if it is not inserted yet
-if {[llength [get_files *${design}.elf]]} {
-	puts "ELF File [get_files *acceptance_test.elf] is already associated"
+# insert elf if it is not inserted yet
+if {[llength [get_files app.elf]]} {
+	puts "ELF File [get_files app.elf] is already associated"
+	exit
 } else {
 	add_files -norecurse -force ${elf_file} 
 	set_property SCOPED_TO_REF [current_bd_design] [get_files -all -of_objects [get_fileset sources_1] ${elf_file}]
@@ -60,6 +64,6 @@ reset_run impl_1 -prev_step
 launch_runs impl_1 -to_step write_bitstream
 wait_on_run impl_1
 open_run impl_1
-write_bitstream -force ../bitfiles/${design}.bit
+write_bitstream -force ../bitfiles/$design.bit
 
 exit
