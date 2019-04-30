7fea8f0cde590d480ef667a1b88c1f72efbbc1fe
Stephen Ibanez <sibanez@g.hmc.edu>
2019-04-29 19:23:29 -0700

    Adding ifconfig ethX up commands to setup script

--- contrib-projects/sume-sdnet-switch/projects/fred_basic/sw/hw_testing/setup/setup-netfpga2.sh
+++ contrib-projects/sume-sdnet-switch/projects/fred_basic/sw/hw_testing/setup/setup-netfpga2.sh
@@ -4,6 +4,8 @@
 ip address add 10.0.0.2/24 dev eth2
 ip address add 10.0.1.2/24 dev eth2
 
+ifconfig eth2 up
+
 # Add static ARP entries
 arp -i eth2 -s 10.0.0.1 0c:c4:7a:b7:5f:92
 arp -i eth2 -s 10.0.1.1 0c:c4:7a:b7:5f:93
